3e0123b9c995b1979091f5b8db5e8bb9ca433458
frostmute <frostmute@gmail.com>
2024-12-08 02:50:01 -0600

    1st

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://frostmute-astro.netlify.app/',
 	markdown: {
 		shikiConfig: {
 		  theme: 'material-theme-ocean',
